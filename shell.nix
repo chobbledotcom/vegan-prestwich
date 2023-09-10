34225f3e14fafff81447f3717a0f34cf64bbff03
Stefan <git@stefn.co.uk>
2023-09-10 12:00:36 +0100

    auto-updated after build

--- shell.nix
+++ shell.nix
@@ -1,20 +1,18 @@
 with (import <nixpkgs> {});
-mkShell {
-  buildInputs = [
-    bundler
-    ruby
-    nodejs
-    nodePackages_latest.npm
-    nodePackages_latest.html-minifier
-  ];
-  shellHook = ''
-    mkdir -p .nix-gems
+  mkShell {
+    buildInputs = [
+      bundler
+      ruby
+      bun
+    ];
+    shellHook = ''
+      mkdir -p .nix-gems
 
-    export GEM_HOME=$PWD/.nix-gems
-    export GEM_PATH=$GEM_HOME
-    export PATH=$GEM_HOME/bin:$PATH
-    export PATH=$PWD/bin:$PATH
+      export GEM_HOME=$PWD/.nix-gems
+      export GEM_PATH=$GEM_HOME
+      export PATH=$GEM_HOME/bin:$PATH
+      export PATH=$PWD/bin:$PATH
 
-    gem list -i ^neocities$ || gem install neocities --no-document
-  '';
-}
+      gem list -i ^neocities$ || gem install neocities --no-document
+    '';
+  }
