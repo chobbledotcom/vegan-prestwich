669058075e5ed64aaabc7b3444df340deb262725
Stefan <git@stefn.co.uk>
2024-11-04 00:45:35 +0000

    Remove gem stuff from shell.nix

--- shell.nix
+++ shell.nix
@@ -8,14 +8,4 @@ with (import <nixpkgs> {});
       nodePackages_latest.npm
       sass
     ];
-    shellHook = ''
-      mkdir -p .nix-gems
-
-      export GEM_HOME=$PWD/.nix-gems
-      export GEM_PATH=$GEM_HOME
-      export PATH=$GEM_HOME/bin:$PATH
-      export PATH=$PWD/bin:$PATH
-
-      gem list -i ^neocities$ || gem install neocities --no-document
-    '';
   }
